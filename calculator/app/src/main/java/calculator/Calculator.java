fa4d18b181d5e3377a0fc618a6d028dd8b10c4be
Valentina <maggio.vale@gmail.com>
2022-06-06 17:30:37 +0200

    TDD calculator program
    - testing a sum method with two arguments using JUnit

---
+++ calculator/app/src/main/java/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class Calculator {
+  public static int add(int x, int y) {
+    return x + y;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(add(2, 2));
+  }
+}
---
+++ calculator/app/src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,13 @@
+package calculator;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+  @Test
+  public void testAddsTwoAndTwo() {
+    Calculator calc = new Calculator();
+    assertEquals("2 + 2 should equal 4", 4, calc.add(2, 2));
+  }
+}
