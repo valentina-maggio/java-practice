130ee7584e6294b08f258f597bab63b69743e971
Valentina <maggio.vale@gmail.com>
2022-06-06 14:32:21 +0200

    Price display program
    - main method with two variables
    - dobule variable formatted with two decimals

---
+++ PriceDisplay.java
@@ -0,0 +1,9 @@
+import java.text.DecimalFormat;
+
+class PriceDisplay {
+  public static void main(String[] args) {
+    String name = "Coffee machine";
+    Double price = 15.99;
+    System.out.printf("The price of the item " + name + " %.2f \n", price);
+  }
+}
